bcc537f09a136b5aec9f66cd4782cdc5cf2d30c4
Jason Breedlove <jbreedlove@jasonbreedlove.dev>
2023-03-31 10:57:11 -0700

    project complete
    deployed to https://message-rosy-two.vercel.app/#c2VjcmV0IG1lc3NhZ2U=

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
+const { hash } = window.location;
+const message = atob(hash.replace("#", ""));
+
+if(message){
+      document.querySelector("#message-form").classList.add("hide");
+      document.querySelector("#message-show").classList.remove("hide");
+
+      document.querySelector("h1").innerHTML = message;
+}
+
+
 document.querySelector("form").addEventListener("submit",  function(event) {
       event.preventDefault();
+      document.querySelector("#message-form").classList.add("hide");
+      document.querySelector("#link-form").classList.remove("hide");
 
-      const input = document.querySelector("input");
-      console.log(input.value);
+      const messageInput = document.querySelector("#message-input");
+      const encrypted = btoa(messageInput.value);
+
+      const urlInput = document.querySelector("#link-input")
+      urlInput.value = `${window.location}#${encrypted}`;
+      urlInput.select();
       })
